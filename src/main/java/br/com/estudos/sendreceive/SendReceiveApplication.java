cf70312d86730612dafa214180c322ae2501c268
Harold Murillo <harold.murillo@spasaude.org.br>
2023-08-24 17:20:31 -0300

    First commit.

---
+++ src/main/java/br/com/estudos/sendreceive/SendReceiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.sendreceive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendReceiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendReceiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudos/sendreceive/SendReceiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.sendreceive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendReceiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
